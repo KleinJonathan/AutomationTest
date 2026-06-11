b3feae8db78ded71651bbe1f7be2ba491610f73f
Jonathan Klein <jnxklein@googlemail.com>
2026-06-11 13:24:43 +0200

    Init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        System.out.println("Starte Test");
+
+        MathClass m = new MathClass();
+        m.setA(12);
+        System.out.println(m.getA());
+
+    }
+}
---
+++ src/test/java/TestMath.java
@@ -0,0 +1,35 @@
+import org.example.MathClass;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestMath {
+
+    @Test
+    public void math_subAB(){
+        MathClass m = new MathClass();
+        m.setA(12);
+        assertEquals(12, m.subAB());
+    }
+
+    @Test
+    public void math_subBA(){
+        MathClass m = new MathClass();
+        m.setA(12);
+        assertEquals(-12, m.subBA());
+    }
+
+    @Test
+    public void math_multiplyWithZero(){
+        MathClass m = new MathClass();
+        m.setA(12);
+        assertEquals(0, m.multAB());
+    }
+
+    @Test
+    public void math_multiply(){
+        MathClass m = new MathClass();
+        m.setA(12);
+        m.setB(2);
+        assertEquals(24, m.multAB());
+    }
+}
