c57f7d6e0fc049fa37ce8faa2406c7efafbe5bac
Jonathan Klein <jnxklein@googlemail.com>
2026-06-22 19:53:18 +0200

    Test CodeQl

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,13 @@
 package org.example;
 
 import org.example.designiteTest.DesigniteDemo;
+import org.example.designiteTest.SqlUserRepository;
 
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-  public static void main() {
+  public static void main(String[] args) {
     System.out.println("Starte Test");
 
     MathClass m = new MathClass();
@@ -19,5 +20,14 @@ public class Main {
 
     // Alle Aufrufe des designiteTest-Pakets
     DesigniteDemo.run();
+
+    // Untrusted input (command-line arguments) flows without validation into SQL queries.
+    // CodeQL treats args as a RemoteFlowSource and reports the SQL injection.
+    if (args.length >= 4) {
+      SqlUserRepository repo = new SqlUserRepository();
+      repo.findUsersByName(args[0]);
+      repo.updateUser(Integer.parseInt(args[1]), args[2], args[3], 30, true);
+      repo.deleteUser(args[1]);
+    }
   }
 }
